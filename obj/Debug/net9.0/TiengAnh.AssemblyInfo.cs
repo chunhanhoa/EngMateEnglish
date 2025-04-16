59b308c285ab4fa336669dbdecdc88634fbb66e1
chunhanhoa <hoachunhan@gmail.com>
2025-04-16 22:12:34 +0700

    Update DACS

--- obj/Debug/net9.0/TiengAnh.AssemblyInfo.cs
+++ obj/Debug/net9.0/TiengAnh.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiengAnh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9899ab0c8120c26fe06df69e3f439827eb0a47ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a501734becf7572b1aedc7f07a5152dbfe1b21a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiengAnh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiengAnh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
